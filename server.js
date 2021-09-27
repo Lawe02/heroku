1ef91f8aaccb83c5fe72ec02dc03c1b73ca1269f
Lawe Zangena <lawza02@gmail.com>
2021-09-27 15:00:40 +0200

    tbx

--- server.js
+++ server.js
@@ -1,40 +1,16 @@
-'use strict';
+'use strict'
 
-// Set access to the http module (provided by node)
-const http = require('http');
+var http = require('http');
 
-// Set access to the fs (file system) module
-const fs = require('fs');
+var fs = require('fs')
 
-// Create a local server that responds & sends data back to the requestor (browser)
 const server = http.createServer();
 
-// Declare a function for what happens when the (incoming) request event triggers
-server.on('request', (req, res) => {
-
-    console.log(req.url);
-    
-    if (req.url == '/Exercise2.js') {
-        fs.readFile('Exercise2.js',function (err, data){
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/javascript');
-            res.setHeader('Content-Length', data.length);
-            res.write(data);
-            res.end();
-        });
-    }
-    else {
-        fs.readFile('index.html',function (err, data){
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.setHeader('Content-Length', data.length);
-            res.write(data);
-            res.end();
-        });
-    }
+http.createServer(function (req, res) {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.write('Hello binary or non binary organisms');
+  res.end();
 });
-
-// Adding a dynamic PORT for Heroku, locally still 8042
 const PORT = process.env.PORT || 8042;
 
 // Have the server start listening locally to the port 8042,
