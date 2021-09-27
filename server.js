caa775d25042cc4336dc462b5e5037423f5d6d4c
Lawe Zangena <lawza02@gmail.com>
2021-09-27 13:55:23 +0200

    initial com

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello binary or non binary organisms!');
+  res.write('Hello binary or non binary organisms');
   res.end();
 }).listen(8080);
