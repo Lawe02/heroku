2c1efe6fb04aadb0d20c272c23c73980a1b4d1d3
Lawe Zangena <lawza02@gmail.com>
2021-09-28 13:15:23 +0200

    some changes

--- server.js
+++ server.js
@@ -1,20 +1,28 @@
 'use strict'
 
-var http = require('http');
+const http = require('http');
 
-var fs = require('fs')
+const fs = require('fs')
 
 const server = http.createServer();
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello binary or non binary organisms');
-  res.end();
+server.on('request',(req,res) => {
+    
+    fs.readFile('index.httml', function (err, data) {
+         
+         res.statusCode = 200;
+         res.setHeader('Content-Length', data.length);
+         res.setHeader('Contetnt type', 'text/html');
+         res.write(data);
+         res.end();
+      });
+
 });
+
+
 const PORT = process.env.PORT || 8042;
 
-// Have the server start listening locally to the port 8042,
-// and with Heroku it will get a dynamic port assigned automatically
+ 
 server.listen(PORT);
 
 console.log(`Server up and listening on port ${PORT}`);
